2b3e4bbd058b1db6aa5129a5920167bdeeb7ede0
Nikita Petrenko <nikita.petrenko@ynov.com>
2025-09-25 11:22:48 +0200

    add style

--- src/main.go
+++ src/main.go
@@ -9,7 +9,13 @@ import (
 
 func main() {
 	initTemp.Init()
+
 	r := router.New()
+
+	// Sert les fichiers statiques (CSS, JS, images, etc.)
+	fs := http.FileServer(http.Dir("./static"))
+	r.Handle("/static/", http.StripPrefix("/static/", fs))
+
 	fmt.Println("Serveur demarré sur http://localhost:8080")
 	http.ListenAndServe(":8080", r)
 }
