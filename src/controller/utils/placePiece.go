bb14bb6a39551f2f6897228a8959dc86a810e27c
KLOTZ alexandre <klotzalexandre431@gmail.com>
2025-09-25 12:32:00 +0200

    placepiece

--- src/controller/utils/placePiece.go
+++ src/controller/utils/placePiece.go
@@ -1,5 +1,22 @@
 package utils
 
-func placePiece() {
+import "power4/controller/structure"
 
+func placePiece(piece int, color string, table *structure.Table) {
+	var x, y int = 0, 0
+	switch piece {
+	case 1:
+		x = 0
+	case 2:
+		x = 70
+	case 3:
+		x = 70 * 2
+	case 4:
+		x = 70 * 3
+	case 5:
+		x = 70 * 4
+	case 6:
+		x = 70 * 5
+	}
+	table.Placement = append(table.Placement, structure.Placement{X: x, Y: y, Color: color})
 }
