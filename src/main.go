2eb424260d88bea618ff21001e49b4aac66b6ab3
Nikita Petrenko <nikita.petrenko@ynov.com>
2025-09-25 09:04:47 +0200

    add readme + first code in main

--- src/main.go
+++ src/main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+	"power4/router"
+)
 
+func main() {
+	r := router.New()
+	fmt.Println("Serveur demarré sur http://localhost:8080")
+	http.ListenAndServe(":8080", r)
 }
